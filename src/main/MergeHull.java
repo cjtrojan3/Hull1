49bab8e873d71b670e8e49f225cf15261295c400
cjtrojan3 <cjtrojan3@gmail.com>
2017-10-15 14:23:41 -0500

    Merge Hull skeleton created.

--- src/main/MergeHull.java
+++ src/main/MergeHull.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Line2D;
 import java.util.Collections;
 import java.util.List;
 import java.util.*;
@@ -35,15 +36,54 @@ public class MergeHull implements ConvexHullFinder{
 	 * @return List of points that are on the hull
 	 */
 	private List<Point2D> recursiveMergeHull(List<Point2D> points){
-		//Divide:
-		//Divide the sorted list into two equal sets
+		//TODO Base Case(s):
+		//Stevenson's write-up says that we have to be careful about these, so stay frosty, Dan
 		
-		//Conquer:
-		//Recursively compute the hull of each subset of points
-		//**Be careful of the base cases**
+//		if(points.size() == 1 || points.size() == 2) {
+//			return points;
+//		}
+		
+		//Divide + Conquer:
+		//Divide the sorted list into two equal sets and compute the hull of them
+		List<Point2D> listA = recursiveMergeHull(points.subList(0, points.size()/2));
+		List<Point2D> listB = recursiveMergeHull(points.subList(points.size()/2, points.size()));
 		
 		//Combine:
 		//Combine the smaller hulls into one larger one
+		
+		//Get the furthest right of left list and the furthest left of the right list
+		//List is supposed to be in CCW order, but might not be sorted from left to right
+		//Stevenson's write-up says that, so I'm finding the points with these for-loops
+		Point2D midLeft = listA.get(0);
+		for(Point2D p : listA) {
+			if(p.getX() > midLeft.getX()) {
+				midLeft = p;
+			}
+		}
+		
+		Point2D midRight = listB.get(0);
+		for(Point2D p : listB) {
+			if(p.getX() < midRight.getX()) {
+				midRight = p;
+			}
+		}
+		
+		Line2D hullConnector = new Line2D.Double(midLeft, midRight);
+		
+		//HARD PART HERE, THOTS
+		//The walking of the tangent line relies heavily on the order of the lists being in CCW order.
+		
+//		A <-- rightmost point of left hull
+//		B <-- leftmost point of right hull
+//		while (T = AB is not the lower tangent to both left and right hulls) {
+//			while (T not lower tangent to left hull) {
+//				A <-- A – 1 (assumption is that hulls are defined in ccw order)
+//			}
+//			while (T not lower tangent to right hull) {
+//				B <-- B + 1 (assumption is that hulls are defined in ccw order)
+//			}
+//		}
+		
 		return null;
 	}
 	
